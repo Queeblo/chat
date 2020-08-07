0debe3edab6b3f1aa7098bdee561f9f6f9606a11
adam figenser <alfigenser@gmail.com>
2020-08-06 21:08:11 -0600

    add username and formatdate helpers

--- client/messageList/messageList.js
+++ client/messageList/messageList.js
@@ -1,6 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { Messages } from '/shared/messages.js';
+import moment from 'moment';
 import './messageList.html';
 
 Template.messageList.onCreated(function () {
@@ -12,4 +13,13 @@ Template.messageList.onCreated(function () {
     messages() {
       return Messages.find();
     },
+    username() {
+      const message = this;
+      const user = Meteor.users.findOne(message.userId);
+      return user.username;
+    },
+    formatDate() {
+      const message = this;
+      return moment(message.date).format('ddd MMM Do h:mm A')
+    }
   });
