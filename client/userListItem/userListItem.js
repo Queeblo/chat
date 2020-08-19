b6aea9b4365e4199775f9e1a7d13f99deaa82807
adam figenser <alfigenser@gmail.com>
2020-08-18 20:59:17 -0600

    render userPopoverContent into popover

---
+++ client/userListItem/userListItem.js
@@ -0,0 +1,20 @@
+import {Blaze} from "meteor/blaze";
+import '../userPopoverContent/userPopoverContent.js';
+import './userListItem.html';
+
+Template.userListItem.onRendered(function () {
+    const user = this.data;
+    const selector = `#${user._id}`;
+
+    $(selector).on('inserted.bs.popover', function () {
+        const parentNode = document.getElementById(`popover-${user._id}`);
+        Blaze.renderWithData(Template.userPopoverContent, user, parentNode);
+    });
+
+    $(selector).popover({
+        content: `<div id="popover-${user._id}"></div>`,
+        title: user.username,
+        placement: "left",
+        html: true,
+    });
+});
