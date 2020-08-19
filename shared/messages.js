ef7b150e3bf1193530fa75fb34124e9264579954
adam figenser <alfigenser@gmail.com>
2020-08-18 21:02:18 -0600

    added directMessage method

--- shared/messages.js
+++ shared/messages.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import {Mongo} from 'meteor/mongo';
+import { Channels } from '../shared/channels.js';
 export const Messages = new Mongo.Collection('messages');
 
 
@@ -15,5 +16,33 @@ Meteor.methods({
     removeMessage(messageId){
         const userId = this.userId;
         return Messages.remove({_id: messageId, userId: userId});
+    },
+    addDirectMessage(message, messagedUserId){
+        const userIds = [message.userId, messagedUserId];
+        const channel = Channels.findOne({userIds: {$all: userIds}});
+        let channelId = null;
+        console.log(userIds, channel);
+        if(!channel){
+            const users = Meteor.users.find(
+                {_id: {$in: userIds}},
+                {fields: {username: 1}}
+                ).fetch();
+            console.log(users);
+            let combinedUsernames = '';
+            users.forEach(function (user){
+                combinedUsernames = `${combinedUsernames} ${user.username}`;
+            })
+            channelId = Channels.insert({
+                name: combinedUsernames,
+                type: 'direct',
+                userIds: userIds,
+            })
+        }else{
+            channelId = channel._id;
+        }
+        Meteor.users.update(message.userId, {$set: {'profile.activeChannel': channelId}});
+        message.channelId = channelId;
+        const id = Messages.insert(message);
+        return id;
     }
 });
