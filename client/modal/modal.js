d89c2d3a82f202279c5f8651f0e69bd9946fd24b
adam figenser <alfigenser@gmail.com>
2020-08-24 20:12:47 -0600

    formatted file

--- client/modal/modal.js
+++ client/modal/modal.js
@@ -1,14 +1,14 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import {Template} from 'meteor/templating';
+import {ReactiveVar} from 'meteor/reactive-var';
 import './modal.html';
 
 export const Modal = {
     templateName: new ReactiveVar(""),
-    open(name){
+    open(name) {
         $('#modal').modal('show');
         this.templateName.set(name);
     },
-    close(){
+    close() {
         this.templateName.set("");
         $('#modal').modal('hide');
     }
@@ -16,12 +16,12 @@ export const Modal = {
 
 Template.modal.helpers({
     template() {
-      return Modal.templateName.get();
+        return Modal.templateName.get();
     }
-  });
+});
 
-  Template.modal.events({
-    'click [data-close]'(event, instance){
-      Modal.close();
+Template.modal.events({
+    'click [data-close]'(event, instance) {
+        Modal.close();
     }
-  })
+})
